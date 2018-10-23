cec579fd256322dbbb08e157f5f674261fe70f2a
Marc Steiner <marc.steiner@belimo.ch>
2018-10-23 13:02:01 +0200

    added new code

--- Java/randomButton/RandomButtonAdvanced.java
+++ Java/randomButton/RandomButtonAdvanced.java
@@ -3,51 +3,82 @@ package randomButton;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
-
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class RandomButtonAdvanced extends Application {
-	List<String> answers = Arrays.asList("Kino","Konzert","Essen gehen");
+	List<String> answers = Arrays.asList();
 	
 	public void start(Stage primaryStage) {
-		VBox root = new VBox(); 
-		root.getChildren().add(createButton());
-			
-		Scene scene = new Scene(root, 500, 500); 
+				
+		GridPane grid = new GridPane();
+		grid.setPadding(new Insets(10,10,10,10));
+		grid.setHgap(10);
+		grid.setVgap(10);
+		
+		Label nameLabel = new Label("Tat:");
+		TextField nameField = new TextField();
+		nameField.setPromptText("Trage deine Tat ein");
+		GridPane.setConstraints(nameLabel, 0 , 0);
+		GridPane.setConstraints(nameField, 1 , 0);
+		
+		Button saveButton = new Button("Save");
+		GridPane.setConstraints(saveButton, 3 , 0);
+		
+		saveButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				addNewAnswer(nameField.getText().toString());
+			}
+		});
+					
+		Button showTextButton = new Button("Klick mich");
+		GridPane.setConstraints(showTextButton, 1 , 1);
+		
+		showTextButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				showTextButton.setText(giveNewAnswer());
+			}
+		});
+		
+		grid.getChildren().addAll(nameLabel, nameField, saveButton, showTextButton);
+	
+		Scene scene = new Scene(grid, 500, 500); 
 		primaryStage.setScene(scene); 
-		primaryStage.setTitle("Hello World"); 
+		primaryStage.setTitle("Random Button Advanced"); 
 		primaryStage.show();
 	}	
 	
-	Pane createButton() {
-		final Button btn = new Button();
-		btn.setText("Was machen wir heute Abend?");
-		final FlowPane pane = new FlowPane();
-		pane.setPadding(new Insets(7, 7, 7, 7));
-		pane.getChildren().add(btn);
-			btn.setOnAction(new EventHandler<ActionEvent>() {
-				@Override
-				public void handle(ActionEvent event) {
-					btn.setText(giveNewAnswer());
-				}
-			});
-		return pane;
-	}
+		public String giveNewAnswer() {
+			Random randomInt= new Random();
+			if (answers.isEmpty()) {
+				return "Leere Liste";
+			}
+			
+			else {
+				return answers.get(randomInt.nextInt(answers.size()));
+			}
+		}
 	
-	public String giveNewAnswer() {
-		Random randomInt= new Random();
-		return answers.get(randomInt.nextInt(answers.size()));
-		
-	}
+		public void addNewAnswer(String input) {
+			if (input.isEmpty() == true || answers.contains(input)) {}
+			
+			else {
+				System.out.println(input);
+				answers.add("Sali");
+			}
+		}
 	
 	public static void main(String[] args) {
 			launch(args);
