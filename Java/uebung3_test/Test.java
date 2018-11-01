01f0926d2e840972470eace4bbdf585839f71a68
Marc Steiner <marc.steiner@belimo.ch>
2018-11-01 16:16:26 +0100

    added code

--- Java/uebung3_test/Test.java
+++ Java/uebung3_test/Test.java
@@ -6,8 +6,15 @@ public class Test {
 		int[] intarr = new int[4];
 		
 		for (int i = 0; i < 8; i++) {
-			intarr[i] = i;
-			System.out.println(intarr[i]);
+			
+			try {
+				intarr[i] = i;
+				System.out.println(intarr[i]);
+			}
+			
+			catch (ArrayIndexOutOfBoundsException e) {
+				System.out.println("Fehler im Array");	
+			}
 		}
 	}
 }
