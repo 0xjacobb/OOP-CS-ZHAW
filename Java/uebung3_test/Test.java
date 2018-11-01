b949be76311362d5962cbdfec64d63caf7f4d8c0
Marc Steiner <marc.steiner@belimo.ch>
2018-11-01 16:01:54 +0100

    added Übung 3 Exception Test

---
+++ Java/uebung3_test/Test.java
@@ -0,0 +1,13 @@
+package uebung3_test;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		int[] intarr = new int[4];
+		
+		for (int i = 0; i < 8; i++) {
+			intarr[i] = i;
+			System.out.println(intarr[i]);
+		}
+	}
+}
