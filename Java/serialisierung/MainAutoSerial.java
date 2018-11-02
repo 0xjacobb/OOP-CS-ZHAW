b79c26909081dd524fb9e223590dda6a4af81452
Marc Steiner <spare@gmx.chr>
2018-11-02 18:50:51 +0100

    added code

---
+++ Java/serialisierung/MainAutoSerial.java
@@ -0,0 +1,28 @@
+package serialisierung;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+
+public class MainAutoSerial {
+
+	public static void main(String[] args) {
+		
+		Auto auto1 = new Auto("Schwarz", new Motor(2000));
+		
+		try (OutputStream fos= new FileOutputStream("Java/serialisierung/auto.ser");
+		ObjectOutputStream oos = new ObjectOutputStream(fos))
+		{
+			oos.writeObject(auto1);
+			oos.close();
+		}
+		
+		catch (IOException e)
+		{
+			e.printStackTrace();
+			
+		}
+	}
+
+}
--- Java/serialisierung/Motor.java
+++ Java/serialisierung/Motor.java
@@ -1,5 +1,21 @@
 package serialisierung;
 
-public class Motor {
+import java.io.Serializable;
 
+public class Motor implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1153032940858150309L;
+	public int hubraum;
+	
+	public Motor(int hubraum){
+		super();
+		this.hubraum = hubraum;
+	}
+
+	public int getHubraum() {
+		return hubraum;
+	}
 }
